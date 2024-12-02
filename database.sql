e8de6fe60c935438d3d1e7c011a415841617b1dd
zLuca1M <lucas.mauricio852@gmail.com>
2024-12-02 16:03:31 -0300

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,28 @@
+-- Tabela Fornecedor
+CREATE TABLE Fornecedor (
+    FornecedorID SERIAL PRIMARY KEY,
+    Nome VARCHAR(100) NOT NULL,
+    Telefone VARCHAR(20),
+    Endereço VARCHAR(255)
+);
+
+-- Tabela Produto
+CREATE TABLE Produto (
+    ProdutoID SERIAL PRIMARY KEY,
+    Nome VARCHAR(100) NOT NULL,
+    Categoria VARCHAR(50),
+    Preço DECIMAL(10, 2) NOT NULL,
+    QuantidadeEstoque INT NOT NULL,
+    FornecedorID INT,
+    FOREIGN KEY (FornecedorID) REFERENCES Fornecedor(FornecedorID)
+);
+
+-- Tabela Movimentação
+CREATE TABLE Movimentação (
+    MovimentaçãoID SERIAL PRIMARY KEY,
+    Tipo VARCHAR(10) CHECK (Tipo IN ('Entrada', 'Saída')),
+    Data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    Quantidade INT NOT NULL,
+    ProdutoID INT,
+    FOREIGN KEY (ProdutoID) REFERENCES Produto(ProdutoID)
+);
